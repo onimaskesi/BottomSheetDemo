bf92d6feb554a1c7b5505920733084f77865918e
Erdem Onat KIRAL <onimaskesi@Erdem-MacBook-Pro.local>
2021-09-02 18:19:13 +0300

    updt

--- src/TextWithHeight.js
+++ src/TextWithHeight.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import {Text, Animated} from 'react-native';
+import {Text} from 'react-native';
 
 const TextWithHeight = ({children, style, height, setHeight}) => {
   return (
     <>
-      {!height && (
-        <Text
-          style={{...style, flex: 1}}
-          onTextLayout={evnt => {
-            const {lines} = evnt.nativeEvent;
-            lines.length > 1 && setHeight((lines.length + 1) * lines[0].height);
-          }}>
-          {children}
-        </Text>
-      )}
-      {!!height && (
-        <Animated.Text style={{...style, height: height}}>
-          {children}
-        </Animated.Text>
-      )}
+      <Text
+        style={[style, !height ? {flex: 1} : {height: height}]}
+        onTextLayout={evnt => {
+          const {lines} = evnt.nativeEvent;
+          lines.length > 1 && setHeight((lines.length + 1) * lines[0].height);
+        }}>
+        {children}
+      </Text>
     </>
   );
 };
