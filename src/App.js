4d79f073d769eb2158412f66218ba5ee64d47860
Erdem Onat KIRAL <onimaskesi@Erdem-MacBook-Pro.local>
2021-09-02 16:24:09 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import BottomSheet from './components/BottomSheet/BottomSheet';
 import Button from './components/Button';
 import CustomCard from './components/CustomCard';
@@ -14,58 +14,63 @@ export default () => {
 
   const [textHeight, setTextHeight] = useState();
 
+  const BottomSheetChildren = () => {
+    return (
+      <View style={{backgroundColor: 'white'}}>
+        <Text style={{backgroundColor: 'orange', color: 'white', fontSize: 15}}>
+          Test
+        </Text>
+        <TextWithHeight
+          height={textHeight}
+          setHeight={setTextHeight}
+          style={{fontSize: 20, padding: 10}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </TextWithHeight>
+        <Text
+          style={{
+            backgroundColor: 'orange',
+            color: 'white',
+            fontSize: 15,
+            padding: 20,
+          }}>
+          Test
+        </Text>
+        <CustomCard />
+        {/* <CustomCard />
+        <CustomCard />
+        <CustomCard /> */}
+      </View>
+    );
+  };
+
+  const ButtonsForOpenBottomSheet = () => {
+    const buttons = [];
+    for (let i = 0; i < 10; i++) {
+      buttons.push(
+        <Button
+          key={i}
+          title="show the bottom sheet"
+          onPress={openBottomSheet}
+        />,
+      );
+    }
+    console.log(buttons);
+    return buttons;
+  };
+
   return (
     <>
-      <SafeAreaView style={{flex: 1}}>
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Button title="show the bottom sheet" onPress={openBottomSheet} />
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Button title="show the bottom sheet" onPress={openBottomSheet} />
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Button title="show the bottom sheet" onPress={openBottomSheet} />
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-        <Text>onimaskesi</Text>
-      </SafeAreaView>
+      <ScrollView style={{flex: 1}}>
+        <ButtonsForOpenBottomSheet />
+      </ScrollView>
       <BottomSheet show={isShowing} setIsShowing={setIsShowing}>
-        <View style={{backgroundColor: 'white'}}>
-          <Text
-            style={{backgroundColor: 'orange', color: 'white', fontSize: 15}}>
-            Test
-          </Text>
-          <TextWithHeight
-            height={textHeight}
-            setHeight={setTextHeight}
-            style={{fontSize: 30, padding: 10}}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </TextWithHeight>
-          <Text
-            style={{
-              backgroundColor: 'orange',
-              color: 'white',
-              fontSize: 15,
-              padding: 20,
-            }}>
-            Test
-          </Text>
-          <CustomCard />
-        </View>
+        <BottomSheetChildren />
       </BottomSheet>
     </>
   );
