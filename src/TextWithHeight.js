d8745b79a255d0f7dbf4b42b1c7580247aacf23d
Erdem Onat KIRAL <onimaskesi@Erdem-MacBook-Pro.local>
2021-09-02 18:23:23 +0300

    updt

--- src/TextWithHeight.js
+++ src/TextWithHeight.js
@@ -5,9 +5,10 @@ const TextWithHeight = ({children, style, height, setHeight}) => {
   return (
     <>
       <Text
-        style={[style, !height ? {flex: 1} : {height: height}]}
+        style={[style, height && {height: height}]}
         onTextLayout={evnt => {
           const {lines} = evnt.nativeEvent;
+          console.log(lines);
           lines.length > 1 && setHeight((lines.length + 1) * lines[0].height);
         }}>
         {children}
