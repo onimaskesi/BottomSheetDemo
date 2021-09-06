9a95ad85f434cf977e96ad9c8a93a8b1b00b9898
Erdem Onat KIRAL <onimaskesi@Erdem-MacBook-Pro.local>
2021-09-06 13:12:22 +0300

    .

--- src/App.js
+++ src/App.js
@@ -47,8 +47,26 @@ export default () => {
   const BottomSheetChildren3 = () => {
     return (
       <View style={{alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={{fontSize: 20, padding: 10, height: 50}}>
-          alksdjfkasdjfjkadslkfjaşlsdkjfşlkjasşdlfkjşsldkjaşfljk
+        <Text style={{fontSize: 20, padding: 10, height: 100}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+        <Button title="Button Title" />
+      </View>
+    );
+  };
+
+  const BottomSheetChildren4 = () => {
+    return (
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{fontSize: 30, padding: 10}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </Text>
         <Button title="Button Title" />
       </View>
