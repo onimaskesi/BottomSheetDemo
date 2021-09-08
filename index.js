b3bb32dc7e7b9f3da1767031483b9f1b7884d620
Erdem Onat KIRAL <onimaskesi@Erdem-MacBook-Pro.local>
2021-09-08 18:56:29 +0300

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 import 'react-native-gesture-handler';
 
 import {AppRegistry} from 'react-native';
-import BottomSheet from './src/BottomSheet';
+import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => BottomSheet);
+AppRegistry.registerComponent(appName, () => App);
